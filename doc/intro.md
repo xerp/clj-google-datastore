e71538e16c2bf39c56d38c69ba7d9310bb0c8f0e
Santiago de Pedro <santiago.pedro23@gmail.com>
2018-09-03 16:34:11 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-google-datastore
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_google_datastore/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-google-datastore.core-test
+  (:require [clojure.test :refer :all]
+            [clj-google-datastore.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
