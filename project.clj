51a84f0584ae308489f0b9bce3c56c3439e543b1
Santiago de Pedro <santiago.pedro23@gmail.com>
2019-06-26 16:35:52 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-google-datastore "2.0"
+(defproject clj-google-datastore "2.5"
   :description "Google datastore api"
   :url "https://github.com/xerp/clj-google-datastore"
   :license {:name "Eclipse Public License"
