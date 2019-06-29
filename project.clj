3d66077cc9696178fd142180a0bab5246b507f2c
Santiago de Pedro <santiago.pedro23@gmail.com>
2019-06-29 12:15:19 -0400

    Update deps

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clj-google-datastore "2.6"
+(defproject clj-google-datastore "2.7"
   :description "Google datastore api"
   :url "https://github.com/xerp/clj-google-datastore"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.cemerick/url "0.1.1"]
-                 [clj-http "3.9.1"]
+                 [clj-http "3.10.0"]
                  [org.clojure/data.json "0.2.6"]
                  [clj-google "1.5"]]
   :repositories [["releases" {:url           "https://clojars.org/repo"
